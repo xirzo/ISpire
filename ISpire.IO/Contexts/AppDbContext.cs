1e845e8e387e1241467a1f672d692565302cd3ee
xirzo <easttem@gmail.com>
2025-08-18 02:09:38 +0300

    feat: postgres connection

---
+++ ISpire.IO/Contexts/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ISpire.IO.Contexts;
+
+public class AppDbContext : DbContext
+{
+    
+}
--- ISpire.Web/Program.cs
+++ ISpire.Web/Program.cs
@@ -1,8 +1,22 @@
+using ISpire.IO.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+
+var connectionStringPath = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+if (connectionStringPath == null)
+{
+    Console.WriteLine("Connection string is not found");
+    return;
+}
+
+var connectionString = File.ReadAllText(connectionStringPath).Trim();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
