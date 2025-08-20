f3bd8c658608d7279c1d2ff288101f659098eba6
xirzo <easttem@gmail.com>
2025-08-20 15:28:09 +0300

    refactor: get db_connection_string inside of the setup

--- ISpire.Web/Program.cs
+++ ISpire.Web/Program.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using ISpire.Core.Repositories;
 using ISpire.Core.Services;
 using ISpire.Infrastructure.Contexts;
@@ -6,23 +5,27 @@ using ISpire.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-
-if (connectionString == null)
-{
-    throw new InvalidOperationException("Database connection string environment variable (DB_CONNECTION_STRING) is not set.");
-}
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+    
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+        throw new InvalidOperationException("DB_CONNECTION_STRING not set.");
+    }
+    
+    options.UseNpgsql(connectionString);
+});
+
+
 
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IAccountRepository, DbAccountRepository>();
 
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
