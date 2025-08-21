7deb3db9269b7bc25545a62191ffbaf94e799157
xirzo <easttem@gmail.com>
2025-08-21 13:30:41 +0300

    migration: permissions added

---
+++ ISpire.Infrastructure/Migrations/20250821102903_permissions_added.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ISpire.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class permissions_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string[]>(
+                name: "Permissions",
+                table: "Accounts",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permissions",
+                table: "Accounts");
+        }
+    }
+}
