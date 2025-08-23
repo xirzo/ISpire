0da030ff706671455b8b0cdeb6b46484cd80fc15
xirzo <easttem@gmail.com>
2025-08-23 22:05:36 +0300

    refactor: add sealed to results

--- ISpire.Core/Errors/RegisterResult.cs
+++ ISpire.Core/Errors/RegisterResult.cs
@@ -4,9 +4,9 @@ namespace ISpire.Core.Errors;
 
 public abstract record RegisterResult
 {
-    public record AlreadyExists : RegisterResult;
-    public record WrongEmailPattern: RegisterResult;
-    public record RepositoryAddFailed: RegisterResult;
-    public record Success(Account Account) : RegisterResult;
-    public record ForbiddenCharactersInName : RegisterResult;
+    public sealed record AlreadyExists : RegisterResult;
+    public sealed record WrongEmailPattern: RegisterResult;
+    public sealed record RepositoryAddFailed: RegisterResult;
+    public sealed record Success(Account Account) : RegisterResult;
+    public sealed record ForbiddenCharactersInName : RegisterResult;
 }
